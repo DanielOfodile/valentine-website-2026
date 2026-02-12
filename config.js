5311d3db063a8670e6c44c5fd75c182397f6d573
Daniel <101346427+DanielOfodile@users.noreply.github.com>
2026-02-13 00:26:18 +0100

    Update music on config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dycp4lrbf/video/upload/v1770938456/Burna_Boy_-_For_My_Hand_Feat._Ed_Sheeran__mp3.pm_phcthl.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
